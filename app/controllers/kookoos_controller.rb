2805df8b1c9e48f699e9a506d96767fc8b8ab971
Aman Kapur <amankapur91@gmail.com>
2012-04-26 17:59:37 +0530

    render test

--- app/controllers/kookoos_controller.rb
+++ app/controllers/kookoos_controller.rb
@@ -5,8 +5,9 @@ class KookoosController < ApplicationController
     @kookoos = Kookoo.all
 
     respond_to do |format|
-      format.html # index.html.erb
-      
+#      format.html # index.html.erb
+
+      render :file => "kookoo_response.xml", :layout => false
     end
   end
 
