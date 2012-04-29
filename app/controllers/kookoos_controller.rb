b7bb965f7d573e615672334f5c5fcb83899c4138
Aman Kapur <amankapur91@gmail.com>
2012-04-29 18:50:40 -0400

    please work

--- app/controllers/kookoos_controller.rb
+++ app/controllers/kookoos_controller.rb
@@ -5,9 +5,13 @@ class KookoosController < ApplicationController
   def index
     
       #render text: "<response><playtext>Welcome to the dispatch rickshaw service.Your location is being gathered. We will dispatch a rickshaw momentarily. Please enjoy some music.</playtext><hangup></hangup></response>"
-      render text: "<response><playtext lang='HI'>
-      वोदाफोन रिक्क्षा सर्विस मे आप्का स्वागत हे आप्की कौल फर्वर्र्द की जा रही हे क्रिपीया प्रतीक्ष कीजीये
-      </playtext><hangup></hangup></response>"
-  end
+      render text: "<?xml version='1.0' encoding='UTF-8'?>
+                    <response filler='yes'>
+                      <playtext lang='HI'>
+                        वोदाफोन रिक्क्षा सर्विस मे आप्का स्वागत हे आप्की कौल फर्वर्र्द की जा रही हे क्रिपीया प्रतीक्ष कीजीये
+                      </playtext>
+                    <hangup></hangup>
+                    </response>"
+    end
 
 end
