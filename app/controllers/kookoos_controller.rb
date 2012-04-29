6f82a0f1b2a29c8b06a8b65f15d082d47e86dc65
Aman Kapur <amankapur91@gmail.com>
2012-04-29 11:07:21 +0530

    eng test2

--- app/controllers/kookoos_controller.rb
+++ app/controllers/kookoos_controller.rb
@@ -1,14 +1,14 @@
 class KookoosController < ApplicationController
   # GET /kookoos
-  # GET /kookoos.json
+# GET /kookoos.json
   def index
-    #respond_to :xml
-    #respond_to do |format|
+   #respond_to :xml
+   # respond_to do |format|
       #format.html
      # format.xml  # index.xml.erb
       #render text: "<response><playtext>Welcome to the dispatch rickshaw service.Your location is being gathered. We will dispatch a rickshaw momentarily. Please enjoy some music.</playtext><hangup></hangup></response>", layout: false
-      render :file => "kookoo_response.xml", :layout => false
-    #end
+      render :file => "kookoo_response.xml"
+   # end
   end
 
 end
