425c209a470e8593fbe2090ec15130e9438f7c53
Aman Kapur <amankapur91@gmail.com>
2012-04-29 18:30:41 -0400

    please work

--- app/controllers/kookoos_controller.rb
+++ app/controllers/kookoos_controller.rb
@@ -1,14 +1,13 @@
+# encoding: utf-8
 class KookoosController < ApplicationController
   # GET /kookoos
 # GET /kookoos.json
   def index
-   #respond_to :xml
-   # respond_to do |format|
-      #format.html
-     # format.xml  # index.xml.erb
-    #  render text: "<response><playtext>Welcome to the dispatch rickshaw service.Your location is being gathered. We will dispatch a rickshaw momentarily. Please enjoy some music.</playtext><hangup></hangup></response>"
-      render :file => "kookoo_response.xml"
-   # end
+    
+      #render text: "<response><playtext>Welcome to the dispatch rickshaw service.Your location is being gathered. We will dispatch a rickshaw momentarily. Please enjoy some music.</playtext><hangup></hangup></response>"
+      render text: "<response><playtext lang='HI'>
+      वोदाफोन रिक्क्षा सर्विस मे आप्का स्वागत हे आप्की कौल फर्वर्र्द की जा रही हे क्रिपीया प्रतीक्ष कीजीये
+      </playtext><hangup></hangup></response>"
   end
 
 end
