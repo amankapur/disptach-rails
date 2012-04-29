c8a91c7c2575fc890be98dd50bf0469aa7084e49
Aman Kapur <amankapur91@gmail.com>
2012-04-29 18:07:29 -0400

    please work

--- app/controllers/kookoos_controller.rb
+++ app/controllers/kookoos_controller.rb
@@ -6,8 +6,8 @@ class KookoosController < ApplicationController
    # respond_to do |format|
       #format.html
      # format.xml  # index.xml.erb
-      #render text: "<response><playtext>Welcome to the dispatch rickshaw service.Your location is being gathered. We will dispatch a rickshaw momentarily. Please enjoy some music.</playtext><hangup></hangup></response>", layout: false
-      render :file => "kookoo_response.xml"
+      render text: "<response><playtext>Welcome to the dispatch rickshaw service.Your location is being gathered. We will dispatch a rickshaw momentarily. Please enjoy some music.</playtext><hangup></hangup></response>"
+     # render :file => "kookoo_response.xml"
    # end
   end
 
